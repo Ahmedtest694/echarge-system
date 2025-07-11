46dfc1d064c2043cee08117572b5b6cc8fab0dd4
Ahmed Reda <haggaga39@gmail.com>
2025-07-11 19:42:29 +0300

    feat: add login button to the UI

---
+++ login.js
@@ -0,0 +1,6 @@
+// This is a login button added to the UI
+function createLoginButton() {
+  const btn = document.createElement("button");
+  btn.textContent = "Login";
+  document.body.appendChild(btn);
+}
